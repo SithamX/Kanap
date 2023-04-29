7ed9b4731d0db6c9a30d57809e4e228c3173ca22
Mathis Méheux <mat.mhx@laposte.net>
2023-04-29 19:51:45 +0200

    Petit avancement pour l'étape 3

--- front/js/script.js
+++ front/js/script.js
@@ -2,12 +2,39 @@
 
 async function data(){
     const ressources = await fetch("http://localhost:3000/api/products");
-    const json = await ressources.json();
-    console.log(json);
+    const products = await ressources.json();
+    console.log(products);
 }
 data();
-//console.log(megaTest);
 
+ /* <a href="./product.html?id=42">
+            <article>
+              <img src="../../back/images/kanap01.jpeg" alt="Lorem ipsum dolor sit amet, Kanap name1">
+              <h3 class="productName">Kanap name1</h3>
+              <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+            </article>
+          </a> */
+
+
+// Création des balises 
+function makeItems(products) { // j'ai créé une fonction et ai mis entre panrenthèses "products" pour pouvoir accéder à la constante products dans la fonction d'au-dessus, puisque les constantes dans les  fonctions sont impossible à y faire référence si je laisse toutes ces autres constantes en dehors de la nouvelle fonction en supprimant la fonction
+    const item = products[0];
+
+    const imageItem = document.createElement("img");
+    imageItem.src = item.image;
+    const nomItem = document.createElement("h3");
+    nomItem.innerText = item.nom;
+    const descriptionItem = document.createElement("p");
+    descritionItem.innerText = item.description;
+
+
+    // Rattachement des balises au DOM
+    const sectionItems = document.querySelector(".items");
+    sectionItems.appendChild(imageItem);
+    sectionItems.appendChild(nomItem);
+    sectionItems.appendChild(descriptionItem);
+    console.log(sectionItems);
+}
 
 //async function loadProduct() {
  //   let result = await fetch("http://localhost:3000/api/products");
