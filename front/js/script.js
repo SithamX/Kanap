39f97aebd95e46276e0551d4062791b7b808688b
Mathis Méheux <mat.mhx@laposte.net>
2023-04-30 21:15:39 +0200

    Bon avancement pour l'étape 3

--- front/js/script.js
+++ front/js/script.js
@@ -1,10 +1,42 @@
 // Récupération des données de l'API
 
 async function data(){
+  // Récupération des élément dans l'API
     const ressources = await fetch("http://localhost:3000/api/products");
     const products = await ressources.json();
     console.log(products);
+
+    // Création d'une boucle for dans la fonction pour permettre d'afficher plusieurs produits avec cette mise en forme plutôt qu'un seul produit et pour permettre de faire référence à la constant "products" qui ne peut être appélée en dehors de la fonction dans laquelle elle à été créée
+    for (let i = 0; i < products.length; i++) { // j'ai créé une fonction et ai mis entre panrenthèses "products" pour pouvoir accéder à la constante products dans la fonction d'au-dessus, puisque les constantes dans les  fonctions sont impossible à y faire référence si je laisse toutes ces autres constantes en dehors de la nouvelle fonction en supprimant la fonction
+    const article = products[0]; //ou products[i] exemple : https://github.com/OpenClassrooms-Student-Center/7697016-Front-End.1/blob/P2C1-BoucleFor/pieces.js tiré du cours : https://openclassrooms.com/fr/courses/7697016-creez-des-pages-web-dynamiques-avec-javascript/7911102-manipulez-les-listes-en-javascript
+    // Récupération de lélément du DOM qui acceuillera les items
+    const sectionItems = document.querySelector(".items");
+    // Création d'une balise a telle que dans l'exemple dans le fichier html
+    const aElement = document.createElement("a");
+    // Création d'une balise article telle que dans l'exemple dans le fichier html
+    const itemElement = document.createElement("article");
+    // Création des différentes balises qui seront dans la balise article ell-même dans la balise a tel que dans l'exemple dans le fichier html
+    const imageItem = document.createElement("img");
+    imageItem.src = article.image;
+    const nomItem = document.createElement("h3");
+    nomItem.innerText = article.nom;
+    const descriptionItem = document.createElement("p");
+    descriptionItem.innerText = article.description;
+    console.log(article);
+
+
+    // Rattachement de la balise <a> a la section items
+    sectionItems.appendChild(aElement);
+    // Rattachement de la balise <article> a la balise <a>>
+    aElement.appendChild(itemElement);
+    // Rattachement des balises se trouvant dans la balise <article>
+    itemElement.appendChild(imageItem);
+    itemElement.appendChild(nomItem);
+    itemElement.appendChild(descriptionItem);
+  
 }
+}
+// Appel de la fonction permettant de l'executer
 data();
 
  /* <a href="./product.html?id=42">
@@ -17,24 +49,8 @@ data();
 
 
 // Création des balises 
-function makeItems(products) { // j'ai créé une fonction et ai mis entre panrenthèses "products" pour pouvoir accéder à la constante products dans la fonction d'au-dessus, puisque les constantes dans les  fonctions sont impossible à y faire référence si je laisse toutes ces autres constantes en dehors de la nouvelle fonction en supprimant la fonction
-    const item = products[0];
-
-    const imageItem = document.createElement("img");
-    imageItem.src = item.image;
-    const nomItem = document.createElement("h3");
-    nomItem.innerText = item.nom;
-    const descriptionItem = document.createElement("p");
-    descritionItem.innerText = item.description;
 
 
-    // Rattachement des balises au DOM
-    const sectionItems = document.querySelector(".items");
-    sectionItems.appendChild(imageItem);
-    sectionItems.appendChild(nomItem);
-    sectionItems.appendChild(descriptionItem);
-    console.log(sectionItems);
-}
 
 //async function loadProduct() {
  //   let result = await fetch("http://localhost:3000/api/products");
