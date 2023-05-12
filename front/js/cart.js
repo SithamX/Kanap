67ecc02095e980746c6a632961f7db0de5c45f39
Mathis Méheux <mat.mhx@laposte.net>
2023-05-12 23:31:12 +0200

    Dans cart.js, il y a une deuxième  manière que j'ai réussi à faire pour afficher les produits dans le panier : mettre la boucle for et fetch dans la fonction et ajouter le html dans le dernier .then tout en pouvant réutiliser l'element prod pour récuperer le nom le prix etc (sans oublier d'enlever le paramètre de la fonction et de l'appeler pour l'exécuter.

--- front/js/cart.js
+++ front/js/cart.js
@@ -12,14 +12,6 @@ let selectionJson = JSON.parse(localStorage.getItem("cart"));
 /*let products = [];
 products.push(selectionJson)*/
 console.log(selectionJson)
-for (let product of selectionJson) {
-    console.log(product.id)
-    console.log(product.quantity)
-    console.log(product.color)
-    fetch(`http://localhost:3000/api/products/${product.id}`)
-        .then(response => response.json())
-        .then((prod) => viewProductsCart(prod));
-
 /*products.push(selectionJson);
 console.log(products)
 console.log(products.color)
@@ -29,42 +21,47 @@ console.log(selectionJson)*/
 
 //console.log(dataCart)s
 
-        function viewProductsCart(prod){
-        
-            
-            
-                console.log(prod.imageUrl)
-            
-                document.querySelector("#cart__items").insertAdjacentHTML(
-                    // Position à l'intérieur de l'élément, après son dernier enfant
-                    "afterbegin",
-                    // Création des balises produits
-                    `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
-                        <div class="cart__item__img">
-                            <img src="${prod.imageUrl}" alt="Photographie d'un canapé">
-                        </div>
-                        <div class="cart__item__content">
-                            <div class="cart__item__content__description">
-                                <h2>${prod.name}</h2>
-                                <p>${product.color}</p>
-                                <p>${prod.price}€</p>
-                            </div>
-                            <div class="cart__item__content__settings">
-                                <div class="cart__item__content__settings__quantity">
-                                    <p>Qté : </p>
-                                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
-                                </div>
-                                <iv class="cart__item__content__settings__delete">
-                                    <p class="deleteItem">Supprimer</p>
-                                </div>
-                            </div>
-                        </div>
-                    </article>`
-                    )
-                
-            
+function viewProductsCart(){
+   
+    for (let product of selectionJson) {
+        console.log(product.id)
+        console.log(product.quantity)
+        console.log(product.color)
+        fetch(`http://localhost:3000/api/products/${product.id}`)
+            .then(response => response.json())
+            .then((prod) => {
+                            console.log(prod.imageUrl)
+                        
+                            document.querySelector("#cart__items").insertAdjacentHTML(
+                                // Position à l'intérieur de l'élément, après son dernier enfant
+                                "afterbegin",
+                                // Création des balises produits
+                                `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
+                                    <div class="cart__item__img">
+                                        <img src="${prod.imageUrl}" alt="Photographie d'un canapé">
+                                    </div>
+                                    <div class="cart__item__content">
+                                        <div class="cart__item__content__description">
+                                            <h2>${prod.name}</h2>
+                                            <p>${product.color}</p>
+                                            <p>${prod.price}€</p>
+                                        </div>
+                                        <div class="cart__item__content__settings">
+                                            <div class="cart__item__content__settings__quantity">
+                                                <p>Qté : </p>
+                                                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+                                            </div>
+                                            <iv class="cart__item__content__settings__delete">
+                                                <p class="deleteItem">Supprimer</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </article>`
+                                )
+            });
         }
 }
+viewProductsCart()
 
 
 /*  <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
